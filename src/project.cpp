360e387c08b20e2f82c7ebc320c562515fa839a5
lada <l.bezdecil@seznam.cz>
2018-12-07 16:24:56 +0100

    Commit test

--- src/project.cpp
+++ src/project.cpp
@@ -6,6 +6,32 @@
  */
 
 #include "simlib.h"
+#include <iostream>
+
+#define SESTRICKY 3
+
+#define ZUBARI 2
+
+#define KRESLA 2
+
+#define KAPACITA_REGISTRACI 100
+
+#define KAPACITA_NAVSTEV 1000
+
+#define NOVY_PACIENT 8
+
+Store Sestricky("Sestricky", SESTRICKY);
+Store Zubari("Zubari", ZUBARI);
+Store Kresla("Kresla", KRESLA);
+Store Kapacita_registraci("Kapacita registraci", KAPACITA_REGISTRACI);
+Store Kapacita_navstev("Kapacita navstev", KAPACITA_NAVSTEV);
+
+Facility Rentgen("Rentgen");
+
+long novy_pacient = 0;
+long odmitnut = 0;
+long prijmuti = 0;
+long v_cekarne = 0;
 
 int main(int argc, char *argv[])
 {
