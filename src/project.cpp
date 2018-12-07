9f922e0d0f877d0453ab878f2ce0bf149b6a7817
lada <l.bezdecil@seznam.cz>
2018-12-07 20:32:19 +0100

    At stavajici pacient

--- src/project.cpp
+++ src/project.cpp
@@ -18,6 +18,8 @@
 
 #define KAPACITA_NAVSTEV 1000
 
+#define STALI_PACIENTI 1000
+
 #define NOVY_PACIENT 8
 
 Store Sestricky("Sestricky", SESTRICKY);
@@ -25,6 +27,7 @@ Store Zubari("Zubari", ZUBARI);
 Store Kresla("Kresla", KRESLA);
 Store Kapacita_registraci("Kapacita registraci", KAPACITA_REGISTRACI);
 Store Kapacita_navstev("Kapacita navstev", KAPACITA_NAVSTEV);
+Store Stali_pacienti("Stali pacienti", STALI_PACIENTI);
 
 Facility Rentgen("Rentgen");
 
@@ -33,15 +36,31 @@ long odmitnut = 0;
 long prijmuti = 0;
 long v_cekarne = 0;
 long pred_zakrokem = 0;
+long prohlidka = 0;
+long tezky_zakrok = 0;
+long lehky_zakrok = 0;
+long konec = 0;
+long hned_objednani = 0;
+long hned_neobjednani = 0;
 
+class Siesta : public Process
+{
+	void Behavior()
+	{
+		Wait(Exponential(0.08333333));
+		Leave(Zubari, 1);
+		this->Terminate();
+	}
+};
 
 class Vstup : public Process
 {
 	void Behavior()
 	{
+		double rand = 0;
 		novy_pacient++;
 
-		hovor:
+hovor:
 
 		Enter(Sestricky, 1);
 
@@ -49,11 +68,12 @@ class Vstup : public Process
 
 		Leave(Sestricky, 1);
 
-		if(!Kapacita_navstev.Empty())
+		if(!Kapacita_registraci.Empty())
 		{
-			Enter(Kapacita_navstev, 1);
+			Enter(Kapacita_registraci, 1);
 			prijmuti++;
 			Wait(Exponential(2160));
+			Leave(Kapacita_registraci, 1);
 
 			if(Random() > 0.15)
 			{
@@ -67,7 +87,6 @@ class Vstup : public Process
 			}
 			else
 			{
-				Leave(Kapacita_navstev, 1);
 				goto vystup;
 			}
 		}
@@ -83,14 +102,54 @@ class Vstup : public Process
 				goto vystup;
 		}
 
-		zakrok:
+zakrok:
 
 		pred_zakrokem++;
 		Enter(Sestricky, 1);
 		Enter(Kresla, 1);
 
-		vystup:
-		Leave(Kapacita_navstev, 1);
+		rand = Random();
+
+		if(rand < 0.2)
+		{
+			Enter(Zubari, 1);
+			Enter(Sestricky, 1);
+			tezky_zakrok++;
+			Wait(Exponential(0.83333333));
+			Leave(Kresla, 1);
+		}
+		else if(rand < 0.5)
+		{
+			Enter(Zubari, 1);
+			prohlidka++;
+			Wait(Exponential(0.33333333));
+			Leave(Kresla, 1);
+			(new Siesta)->Activate();
+			goto bez_placeni;
+		}
+		else
+		{
+			lehky_zakrok++;
+			Wait(Exponential(0.5));
+			Leave(Kresla, 1);
+		}
+		(new Siesta)->Activate();
+		Wait(Exponential(0.08333333));
+bez_placeni:
+
+		if(Random() < 0.2)
+		{
+			hned_neobjednani++;
+			Leave(Sestricky, 1);
+			Enter(Stali_pacienti, 1);
+		}
+		else
+		{
+			hned_objednani++;
+			Wait(Exponential(0.16666667));
+		}
+vystup:
+		konec++;
 
 	}
 };
